e3fbb714d55f21ad4a1f11a0a13e4b58dbfdc377
szr712 <zirui990712@163.com>
2021-08-05 12:48:48 +0800

    修改try

--- request_google.py
+++ request_google.py
@@ -22,45 +22,48 @@ def requset_google(py_list, tones_list):
         'ie': "utf-8",
         'oe': "utf-8' -H 'content-length:0'",
     }
+    
     flag = 1
-    while flag:
+    while flag!=0 and flag<=10:
         try:
             # response = requests.post(
             #    url='https://inputtools.google.com/request', data=data_dict, proxies=proxies)
             response = requests.post(
                 url='https://inputtools.google.com/request', data=data_dict)
+            candidates = response.json()[1][0][1]
+            result = candidates[0]
+            result_pinyin = pinyin(result, style=Style.TONE3)
+            result_tones = [int(py[0][-1]) if py[0][-1].isdigit() else 0 for py in result_pinyin]
+
+            if not tone_equal(result_tones, tones_list):
+                for j in range(1, len(candidates)):
+                    candidate = candidates[j].replace('\'', '')
+                    pre_len = len(candidate)
+                    if not tone_equal(result_tones[:pre_len], tones_list[:pre_len]):
+                        candidate_pinyin = pinyin(candidate, style=Style.TONE3)
+                        candidate_tones = [int(py[0][-1]) if py[0][-1].isdigit() else 0 for py in candidate_pinyin]
+                        if tone_equal(candidate_tones, tones_list[:pre_len]):
+                            result = candidate
+                            break
+
             flag = 0
         except Exception as e:
+            flag+=1
             traceback.print_exc()
             print("Error")
-            time.sleep(1)
-    try:        
-        candidates = response.json()[1][0][1]
-        result = candidates[0]
-    except:
-        print(response.json())
-        print(py_list)
+            print(flag)
+            if flag==10: return ""
+            time.sleep(1)     
+    
 
-    result_pinyin = pinyin(result, style=Style.TONE3)
-    result_tones = [int(py[0][-1]) if py[0][-1].isdigit() else 0 for py in result_pinyin]
-
-    if not tone_equal(result_tones, tones_list):
-        for j in range(1, len(candidates)):
-            candidate = candidates[j].replace('\'', '')
-            pre_len = len(candidate)
-            if not tone_equal(result_tones[:pre_len], tones_list[:pre_len]):
-                candidate_pinyin = pinyin(candidate, style=Style.TONE3)
-                candidate_tones = [int(py[0][-1]) if py[0][-1].isdigit() else 0 for py in candidate_pinyin]
-                if tone_equal(candidate_tones, tones_list[:pre_len]):
-                    result = candidate
-                    break
-
-    return result + requset_google(py_list[len(result):], tones_list[len(result):])
+    return result + requset_google(py_list[len(result):], tones_list[len(result):]) 
 
 
 
 if __name__ == "__main__":
     string = "究发现酸的浓度越大，产生气体的速度越快，与甲比较，对丁分析正确的是    （填编号）。"
+    
     pinyin_list,tones_list=wenzi2pinyin(string)
+    # pinyin_list=['ben', 'ti', 'kao', 'cha', 'de', 'zhi', 'shi', 'dian', 'wei', 'lu', 'di', 'zi', 'ran', 'dai', 'de', 'fen', 'bu']
     # print(pinyin_list)
     print(requset_google(pinyin_list,tones_list))
